58ebfea4e2f766b13e5f64a16d397c4f03c0bbeb
Le-Doyen <richard.cambe@outlook.fr>
2023-12-04 17:28:51 +0100

    update all

--- assets/monster.py
+++ assets/monster.py
@@ -14,7 +14,13 @@ class Monster(pygame.sprite.Sprite) :
         self.rect.y = 559
         self.velocity = 1
 
-    
+    def update_health_bar(self, surface):
+        #definir une couleur pour la jauge de vie (rouge)
+        bar_color = (170, 37, 32)
+        #definir la position de la jauge de vie + largeur + epaisseur
+        bar_position = [self.rect.x, self.rect.y, self.health, 5]
+        #dessiner la jauge de vie
+        pygame.draw.rect(surface, bar_color, bar_position)
 
 
     def forward(self):
--- assets/sword.py
+++ assets/sword.py
@@ -5,7 +5,7 @@ class Sword(pygame.sprite.Sprite) :
     def __init__(self, game):
         super().__init__()
         self.game = game
-        self.image = pygame.image.load('Hackathon/GameJam2023_TheNorthernRegalia/src/sword.png')
+        self.image = pygame.image.load('./src/sword.png')
         self.rect = self.image.get_rect()
         self.rect.x = 425
         self.rect.y = 559
